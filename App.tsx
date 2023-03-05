73a99c2228f209d2f400a719e7345c6b3ce08197
Carl Meyer <carl@ukiyo-studio.co.uk>
2023-03-05 18:27:00 +0000

    Add navigation
    - add tab navigation to app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import Navigation from './src/navigation/navigation';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white dark: bg-black">
-      <Text className='color-black dark: color-white'>Mini Break App 🚀</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation></Navigation>
   );
 }
